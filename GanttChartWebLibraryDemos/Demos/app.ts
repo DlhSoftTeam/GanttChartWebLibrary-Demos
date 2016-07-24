1276109aeb1d84adb4c03c2155bfa9738f38dbb3
DlhSoft <Contact@DlhSoft.com>
2016-07-24 15:24:44 +0300

    Initial commit

---
+++ GanttChartWebLibraryDemos/Demos/app.ts
@@ -0,0 +1,202 @@
+﻿declare var angular;
+
+var queryString = window.location.search;
+var initialSelection = queryString ? queryString.substr(1).replace('-', ' ') : null;
+
+interface Sample {
+    component: string;
+    feature: string;
+    title: string;
+    description: string;
+    sourceCodeFiles?: { [key: string]: string[] };
+    sourceCodeUrls?: { [key: string]: string };
+}
+
+angular.module('Demos', [])
+    .controller('MainController', ($scope, $http, $timeout) => {
+        var components = ['GanttChartView', 'ScheduleChartView', 'LoadChartView', 'PertChartView', 'NetworkDiagramView'];
+        var samples = <Sample[]>[
+            {
+                component: 'GanttChartView', feature: 'MainFeatures', title: 'Main features', description: 'Complex sample application showing how to use the most important features of the component',
+                sourceCodeFiles: {
+                    'CSharp': ['index.html', 'app.css', 'app.js', 'templates.js', 'themes.js'],
+                    'VisualBasic': ['index.html', 'app.css', 'app.ts', 'templates.js', 'themes.js']
+                }
+            },
+            {
+                component: 'ScheduleChartView', feature: 'MainFeatures', title: 'Main features', description: 'Complex sample application showing how to use the most important features of the component',
+                sourceCodeFiles: {
+                    'CSharp': ['index.html', 'app.css', 'app.js', 'templates.js', 'themes.js']
+                }
+            },
+            {
+                component: 'LoadChartView', feature: 'MainFeatures', title: 'Main features', description: 'Complex sample application showing how to use the most important features of the component',
+                sourceCodeFiles: {
+                    'CSharp': ['index.html', 'app.css', 'app.js', 'themes.js']
+                }
+            },
+            {
+                component: 'LoadChartView', feature: 'SingleItem', title: 'Single item', description: 'Sample application showing how to display a single item with multiple allocations',
+                sourceCodeFiles: {
+                    'CSharp': ['index.html', 'app.css', 'app.js']
+                }
+            },
+            {
+                component: 'PertChartView', feature: 'MainFeatures', title: 'Main features', description: 'Complex sample application showing how to use the most important features of the component',
+                sourceCodeFiles: {
+                    'CSharp': ['index.html', 'app.css', 'app.js', 'themes.js']
+                }
+            },
+            {
+                component: 'NetworkDiagramView', feature: 'MainFeatures', title: 'Main features', description: 'Complex sample application showing how to use the most important features of the component',
+                sourceCodeFiles: {
+                    'CSharp': ['index.html', 'app.css', 'app.js', 'themes.js']
+                }
+            }
+        ];
+        var themes = ['Default', 'Generic-bright', 'Generic-blue', 'DlhSoft-gray', 'Purple-green', 'Steel-blue', 'Dark-black', 'Cyan-green', 'Blue-navy', 'Orange-brown', 'Teal-green', 'Purple-beige', 'Gray-blue', 'Aero'];
+        $scope.themes = themes;
+        $scope.selectedTheme = themes[1];
+        $scope.selectTheme = (theme) => {
+            if (theme == $scope.selectedTheme)
+                return;
+            $scope.applyingTheme = theme;
+            $scope.selectedTheme = null;
+            $timeout(() => {
+                $scope.selectedTheme = theme;
+                $scope.applyingTheme = null;
+                $scope.run();
+            });
+        };
+        var technologies = [{ name: 'CSharp', title: 'C# ASPX + JS + HTML' }, { name: 'VisualBasic', title: 'Visual Basic® ASPX' }];
+        $scope.technologies = technologies;
+        $scope.selectedTechnology = technologies[0];
+        var getSamples = (component, selectedTechnology) => {
+            var componentSamples = [];
+            for (var i = 0; i < samples.length; i++) {
+                var sample = samples[i];
+                if (sample.component == component &&
+                    ((sample.sourceCodeFiles && sample.sourceCodeFiles[selectedTechnology.name]) ||
+                     (sample.sourceCodeUrls && sample.sourceCodeUrls[selectedTechnology.name])))
+                    componentSamples.push(sample);
+            }
+            return componentSamples;
+        };
+        var getComponents = (selectedTechnology) => {
+            var components = [];
+            for (var i = 0; i < samples.length; i++) {
+                var sample = samples[i];
+                var component = sample.component;
+                if (components.indexOf(component) < 0 &&
+                    ((sample.sourceCodeFiles && sample.sourceCodeFiles[selectedTechnology.name]) ||
+                     (sample.sourceCodeUrls && sample.sourceCodeUrls[selectedTechnology.name])))
+                    components.push(component);
+            }
+            return components;
+        };
+        var selectSample = (sample) => {
+            $scope.selectedSample = sample;
+            $scope.run();
+        };
+        var selectComponent = (component) => {
+            var firstComponentSample;
+            for (var i = 0; i < samples.length; i++) {
+                var sample = samples[i];
+                if (sample.component == component) {
+                    if (sample.feature == $scope.selectedSample.feature) {
+                        selectSample(sample);
+                        return;
+                    }
+                    if (!firstComponentSample)
+                        firstComponentSample = sample;
+                }
+            }
+            selectSample(firstComponentSample);
+        };
+        $scope.selectTechnology = (technology) => {
+            if (technology == $scope.selectedTechnology)
+                return;
+            $scope.selectedTechnology = technology;
+            var selectedSample = $scope.selectedSample;
+            var selectedComponent = selectedSample.component;
+            var selectedFeature = selectedSample.feature;
+            if (getComponents(technology).indexOf(selectedComponent) < 0)
+                selectComponent(selectedComponent = components[0]);
+            var componentSamples = getSamples(selectedComponent, technology);
+            var featureSampleFound = false;
+            for (var i = 0; i < componentSamples.length; i++) {
+                var sample = componentSamples[i];
+                if (sample.feature == selectedFeature && sample.sourceCodeFiles && sample.sourceCodeFiles[technology.name]) {
+                    featureSampleFound = true;
+                    selectSample(sample);
+                    break;
+                }
+            }
+            if (!featureSampleFound)
+                selectSample(componentSamples[0]);
+            $scope.run();
+        };
+        $scope.components = components;
+        $scope.samples = samples;
+        $scope.selectedSample = samples[0];
+        $scope.getComponents = getComponents;
+        $scope.getSamples = getSamples;
+        $scope.selectSample = selectSample;
+        $scope.selectComponent = selectComponent;
+        $scope.getSourceCodeFiles = (selectedSample, selectedTechnology) => {
+            return selectedSample.sourceCodeFiles ? selectedSample.sourceCodeFiles[selectedTechnology.name] : null;
+        };
+        $scope.selectedSourceCodeFile = null;
+        $scope.selectedSourceCodeFileContents = null;
+        $scope.selectSourceCodeFile = (selectedSample, selectedTechnology, sourceCodeFile) => {
+            $scope.selectedSourceCodeFile = sourceCodeFile;
+            $scope.selectedSourceCodeFileContents = '…';
+            var sourceCodeFileUrl = 'Samples/' + selectedTechnology.name + '/' + selectedSample.component + '/' + selectedSample.feature + '/' + sourceCodeFile.replace('.aspx', '.aspx.txt').replace('.aspx.txt.cs', '.aspx.cs.txt');
+            $http.get(sourceCodeFileUrl).then((response) => {
+                $scope.selectedSourceCodeFileContents = response.data;
+            });
+        };
+        $scope.forceRun = false;
+        $scope.run = (allowRefreshing) => {
+            if (allowRefreshing && $scope.selectedSourceCodeFile == null) {
+                var technology = $scope.selectedTechnology;
+                $scope.selectedTechnology = null;
+                $timeout(() => {
+                    $scope.selectedTechnology = technology;
+                });
+            }
+            $scope.selectedSourceCodeFile = null;
+            $scope.selectedSourceCodeFileContents = null;
+        };
+        $scope.getSampleUrl = (selectedSample, selectedTechnology, selectedTheme) => {
+            return 'Samples/' + (selectedTechnology ? selectedTechnology.name : '') + '/' + selectedSample.component + '/' + selectedSample.feature + '/index.html?' + (selectedTheme ? selectedTheme : $scope.applyingTheme);
+        };
+        if (initialSelection)
+            selectComponent(initialSelection);
+    })
+    .directive('dsSample', ($timeout) => {
+        return {
+            restrict: 'E',
+            replace: true,
+            bindToController: {
+                html: '='
+            },
+            controller: ($scope) => {
+            },
+            controllerAs: 'dss',
+            templateUrl: 'Templates/Sample.html'
+        };
+    })
+    .directive('dsSourceCode', ($timeout) => {
+        return {
+            restrict: 'E',
+            replace: true,
+            bindToController: {
+                contents: '='
+            },
+            controller: ($scope) => {
+            },
+            controllerAs: 'dssc',
+            templateUrl: 'Templates/SourceCode.html'
+        };
+    });

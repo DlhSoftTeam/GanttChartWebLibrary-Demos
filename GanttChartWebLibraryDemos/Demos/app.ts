f851c50c95a0d944de225d620d616cb51e79e313
DlhSoft <Contact@DlhSoft.com>
2016-07-24 23:25:56 +0300

    Updated DlhSoft packages

--- GanttChartWebLibraryDemos/Demos/app.ts
+++ GanttChartWebLibraryDemos/Demos/app.ts
@@ -38,7 +38,7 @@ angular.module('Demos', [])
                 $scope.run();
             });
         };
-        var technologies = [{ name: 'CSharp', title: 'C# + Forms' }, { name: 'VisualBasic', title: 'Visual Basic®' }];
+        var technologies = [{ name: 'CSharp', title: 'C#' }, { name: 'VisualBasic', title: 'Visual Basic®' }];
         $scope.technologies = technologies;
         $scope.selectedTechnology = technologies[0];
         var getSamples = (component, selectedTechnology) => {
